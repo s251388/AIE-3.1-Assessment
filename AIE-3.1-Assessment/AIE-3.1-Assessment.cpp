18d01e5bdcbd40fe2c0b7348cd37010ff46790e5
s251388 <s251388@students.aie.edu.au>
2026-03-25 10:56:01 +1100

    Added all Vector4 operators
    Took alot of renaming, a classmate showed me a waayyyyy easier way to select all the places i want to change/write code (Ctrl + Alt + Click-Drag)

--- AIE-3.1-Assessment/AIE-3.1-Assessment.cpp
+++ AIE-3.1-Assessment/AIE-3.1-Assessment.cpp
@@ -5,230 +5,443 @@
 using namespace std;
 
 namespace MathLibrary {
-    
-    struct Vector3 {
-        float x;
-        float y;
-        float z;
+	
+	struct Vector3 {
 
-        Vector3 operator+(const Vector3& op) {
+		float x, y, z;
 
-            Vector3 temp;
-            temp.x = x + op.x;
-            temp.y = y + op.y;
-            temp.z = z + op.z;
+		Vector3 operator+(const Vector3& op) {
 
-            return temp;
-        }
+			Vector3 temp;
+			temp.x = x + op.x;
+			temp.y = y + op.y;
+			temp.z = z + op.z;
 
-        Vector3 operator-(const Vector3& op) {
+			return temp;
+		}
 
-            Vector3 temp;
-            temp.x = x - op.x;
-            temp.y = y - op.y;
-            temp.z = z - op.z;
+		Vector3 operator-(const Vector3& op) {
 
-            return temp;
-        }
+			Vector3 temp;
+			temp.x = x - op.x;
+			temp.y = y - op.y;
+			temp.z = z - op.z;
 
-        Vector3 operator*(const Vector3& op) {
+			return temp;
+		}
 
-            Vector3 temp;
-            temp.x = x * op.x;
-            temp.y = y * op.y;
-            temp.z = z * op.z;
+		Vector3 operator*(const Vector3& op) {
 
-            return temp;
-        }
+			Vector3 temp;
+			temp.x = x * op.x;
+			temp.y = y * op.y;
+			temp.z = z * op.z;
 
-        Vector3 operator*(const float& op) {
+			return temp;
+		}
 
-            Vector3 temp;
-            temp.x = x * op;
-            temp.y = y * op;
-            temp.z = z * op;
+		Vector3 operator*(const float& op) {
 
-            return temp;
-        }
+			Vector3 temp;
+			temp.x = x * op;
+			temp.y = y * op;
+			temp.z = z * op;
 
-        Vector3 operator/(const float& op) {
+			return temp;
+		}
 
-            Vector3 temp;
-            temp.x = x / op;
-            temp.y = y / op;
-            temp.z = z / op;
+		Vector3 operator/(const float& op) {
 
-            return temp;
-        }
+			Vector3 temp;
+			temp.x = x / op;
+			temp.y = y / op;
+			temp.z = z / op;
 
-        Vector3 operator=(const Vector3& op) {
+			return temp;
+		}
 
-            x = op.x;
-            y = op.y;
-            z = op.z;
+		Vector3 operator=(const Vector3& op) {
 
-            return *this;
-        }
+			x = op.x;
+			y = op.y;
+			z = op.z;
 
-        Vector3 operator+=(const Vector3& op) {
+			return *this;
+		}
 
-            x += op.x;
-            y += op.y;
-            z += op.z;
+		Vector3 operator+=(const Vector3& op) {
 
-            return *this;
-        }
+			x += op.x;
+			y += op.y;
+			z += op.z;
 
-        Vector3 operator-=(const Vector3& op) {
+			return *this;
+		}
 
-            x -= op.x;
-            y -= op.y;
-            z -= op.z;
+		Vector3 operator-=(const Vector3& op) {
 
-            return *this;
-        }
+			x -= op.x;
+			y -= op.y;
+			z -= op.z;
 
-        Vector3 operator*=(const Vector3& op) {
+			return *this;
+		}
 
-            x *= op.x;
-            y *= op.y;
-            z *= op.z;
+		Vector3 operator*=(const Vector3& op) {
 
-            return *this;
-        }
+			x *= op.x;
+			y *= op.y;
+			z *= op.z;
 
-        Vector3 operator*=(const float& op) {
+			return *this;
+		}
 
-            x *= op;
-            y *= op;
-            z *= op;
+		Vector3 operator*=(const float& op) {
 
-            return *this;
-        }
+			x *= op;
+			y *= op;
+			z *= op;
 
-        Vector3 operator/=(const float& op) {
+			return *this;
+		}
 
-            x /= op;
-            y /= op;
-            z /= op;
+		Vector3 operator/=(const float& op) {
 
-            return *this;
-        }
+			x /= op;
+			y /= op;
+			z /= op;
 
-        Vector3 operator-() {
+			return *this;
+		}
 
-            x *= -1;
-            y *= -1;
-            z *= -1;
+		Vector3 operator-() {
 
-            return *this;
-        }
+			x *= -1;
+			y *= -1;
+			z *= -1;
 
-        bool operator==(const Vector3& op) {
-            return (x == op.x && y == op.y && z == op.z);
-        }
+			return *this;
+		}
 
-        bool operator!=(const Vector3& op) {
-            return (x != op.x || y != op.y || z != op.z);
-        }
-        
-        bool operator<(const Vector3& op) {
+		bool operator==(const Vector3& op) {
+			return (x == op.x && y == op.y && z == op.z);
+		}
 
-            Vector3 temp1 = *this;
-            temp1 *= *this;
-            float mag1 = (temp1.x + temp1.y + temp1.z);        // Gets the XYZ values squared and added. The two magnitudes don't need to be
-                                                               // square-rooted back down because they are BOTH squared, their relationship
-            Vector3 temp2 = op;                                // to eachother will be the same no matter what i multiply or divide them by.
-            temp2 *= op;
-            float mag2 = (temp2.x + temp2.y + temp2.z);
+		bool operator!=(const Vector3& op) {
+			return (x != op.x || y != op.y || z != op.z);
+		}
+		
+		bool operator<(const Vector3& op) {
 
-            return (mag1 < mag2);
-        }
+			Vector3 temp1 = *this;
+			temp1 *= *this;
+			float mag1 = (temp1.x + temp1.y + temp1.z);        // Gets the XYZ values squared and added. The two magnitudes don't need to be
+															   // square-rooted back down because they are BOTH squared, their relationship
+			Vector3 temp2 = op;                                // to eachother will be the same no matter what i multiply or divide them by.
+			temp2 *= op;
+			float mag2 = (temp2.x + temp2.y + temp2.z);
 
-        float operator[](int i) {          // Originally i used switch cases but this felt more efficient, i make a temporary vector of the
-                                           // current Vector3's XYZ values, then just use the input int to return the float at that index.
-            vector<float> XYZ {x,y,z};
-            
-            return XYZ[i];
+			return (mag1 < mag2);
+		}
 
-        }
+		float operator[](int i) {          // Originally i used switch cases but this felt more efficient, i make a temporary vector of the
+										   // current Vector3's XYZ values, then just use the input int to return the float at that index.
+			vector<float> XYZ {x,y,z};
+			
+			return XYZ[i];
 
+		}
 
-    };
+	};
 
-    struct Vector4 {
-        float x;
-        float y;
-        float z;
-        float w;
-    };
+	struct Vector4 {
 
-    struct Matrix3 {
+		float x, y, z, w;
 
-    };
+		Vector4 operator+(const Vector4& op) {
+
+			Vector4 temp;
+			temp.x = x + op.x;
+			temp.y = y + op.y;
+			temp.z = z + op.z;
+			temp.w = w + op.w;
+
+			return temp;
+		}
+
+		Vector4 operator-(const Vector4& op) {
+
+			Vector4 temp;
+			temp.x = x - op.x;
+			temp.y = y - op.y;
+			temp.z = z - op.z;
+			temp.w = w - op.w;
+
+			return temp;
+		}
+
+		Vector4 operator*(const Vector4& op) {
+
+			Vector4 temp;
+			temp.x = x * op.x;
+			temp.y = y * op.y;
+			temp.z = z * op.z;
+			temp.w = w * op.w;
+
+			return temp;
+		}
+
+		Vector4 operator*(const float& op) {
+
+			Vector4 temp;
+			temp.x = x * op;
+			temp.y = y * op;
+			temp.z = z * op;
+			temp.w = w * op;
+
+			return temp;
+		}
+
+		Vector4 operator/(const float& op) {
+
+			Vector4 temp;
+			temp.x = x / op;
+			temp.y = y / op;
+			temp.z = z / op;
+			temp.w = w / op;
+
+			return temp;
+		}
+
+		Vector4 operator=(const Vector4& op) {
+
+			x = op.x;
+			y = op.y;
+			z = op.z;
+			w = op.w;
+
+			return *this;
+		}
+
+		Vector4 operator+=(const Vector4& op) {
+
+			x += op.x;
+			y += op.y;
+			z += op.z;
+			w += op.w;
+
+			return *this;
+		}
+
+		Vector4 operator-=(const Vector4& op) {
+
+			x -= op.x;
+			y -= op.y;
+			z -= op.z;
+			w -= op.w;
+
+			return *this;
+		}
+
+		Vector4 operator*=(const Vector4& op) {
+
+			x *= op.x;
+			y *= op.y;
+			z *= op.z;
+			w *= op.w;
+
+			return *this;
+		}
+
+		Vector4 operator*=(const float& op) {
+
+			x *= op;
+			y *= op;
+			z *= op;
+			w *= op;
+
+			return *this;
+		}
+
+		Vector4 operator/=(const float& op) {
+
+			x /= op;
+			y /= op;
+			z /= op;
+			w /= op;
+
+			return *this;
+		}
+
+		Vector4 operator-() {
+
+			x *= -1;
+			y *= -1;
+			z *= -1;
+			w *= -1;
+
+			return *this;
+		}
+
+		bool operator==(const Vector4& op) {
+			return (x == op.x && y == op.y && z == op.z && w == op.w);
+		}
+
+		bool operator!=(const Vector4& op) {
+			return (x != op.x || y != op.y || z != op.z || w != op.w);
+		}
+
+		bool operator<(const Vector4& op) {
+
+			Vector4 temp1 = *this;
+			temp1 *= *this;
+			float mag1 = (temp1.x + temp1.y + temp1.z + temp1.w);        // Gets the XYZ values squared and added. The two magnitudes don't need to be
+			// square-rooted back down because they are BOTH squared, their relationship
+			Vector4 temp2 = op;                                // to eachother will be the same no matter what i multiply or divide them by.
+			temp2 *= op;
+			float mag2 = (temp2.x + temp2.y + temp2.z + temp2.w);
+
+			return (mag1 < mag2);
+		}
+
+		float operator[](int i) {          // Originally i used switch cases but this felt more efficient, i make a temporary vector of the
+			// current Vector3's XYZ values, then just use the input int to return the float at that index.
+			vector<float> XYZW{x,y,z,w};
+
+			return XYZW[i];
+
+		}
+
+	};
+
+	struct Matrix3 {
+
+	};
 
 }
 
 
 int main()
 {
-    
-    MathLibrary::Vector3 Vec1{1,2,3};
-    MathLibrary::Vector3 Vec2{4,5,6};
-    MathLibrary::Vector3 Vec3{};
+	
+	MathLibrary::Vector3 Pos1{1,2,3};
+	MathLibrary::Vector3 Pos2{4,5,6};
+	MathLibrary::Vector3 Pos3{};
 
-    Vec3 = Vec1 + Vec2;
-    cout << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  +  " << Vec2.x << "," << Vec2.y << "," << Vec2.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = Pos1 + Pos2;
+	cout << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  +  " << Pos2.x << "," << Pos2.y << "," << Pos2.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = Vec1 - Vec2;
-    cout << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  -  " << Vec2.x << "," << Vec2.y << "," << Vec2.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = Pos1 - Pos2;
+	cout << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  -  " << Pos2.x << "," << Pos2.y << "," << Pos2.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = Vec1 * Vec2;
-    cout << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  *  " << Vec2.x << "," << Vec2.y << "," << Vec2.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = Pos1 * Pos2;
+	cout << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  *  " << Pos2.x << "," << Pos2.y << "," << Pos2.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = Vec1 * 5;
-    cout << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  *  " << "5f" << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = Pos1 * 5;
+	cout << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  *  " << "5f" << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = Vec1 / 5;
-    cout << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  /  " << "5f" << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = Pos1 / 5;
+	cout << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  /  " << "5f" << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
 
-    cout << "Vec3 from here on will always be set to 1,1,1 before operations." << endl;
+	cout << "Pos3 from here on will always be set to 1,1,1 before operations." << endl;
 
-    Vec3 = {1,1,1};
-    Vec3 += Vec1;
-    cout << " +=  " << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	Pos3 += Pos1;
+	cout << " +=  " << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    Vec3 -= Vec1;
-    cout << " -=  " << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	Pos3 -= Pos1;
+	cout << " -=  " << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    Vec3 *= Vec1;
-    cout << " *=  " << Vec1.x << "," << Vec1.y << "," << Vec1.z << "  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	Pos3 *= Pos1;
+	cout << " *=  " << Pos1.x << "," << Pos1.y << "," << Pos1.z << "  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    Vec3 *= 5;
-    cout << " *=  5  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	Pos3 *= 5;
+	cout << " *=  5  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    Vec3 /= 5;
-    cout << " /=  5  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	Pos3 /= 5;
+	cout << " /=  5  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    -Vec3;
-    cout << " -Vec  =  " << Vec3.x << "," << Vec3.y << "," << Vec3.z << endl;
+	Pos3 = {1,1,1};
+	-Pos3;
+	cout << " -Pos  =  " << Pos3.x << "," << Pos3.y << "," << Pos3.z << endl;
 
-    Vec3 = {1,1,1};
-    cout << "Vec3  ==  Vec1  =  " << (Vec3 == Vec1) << endl;
+	Pos3 = {1,1,1};
+	cout << "Pos3  ==  Pos1  =  " << (Pos3 == Pos1) << endl;
 
-    Vec3 = {1,1,1};
-    cout << "Vec3  !=  Vec1  =  " << (Vec3 != Vec1) << endl;
+	Pos3 = {1,1,1};
+	cout << "Pos3  !=  Pos1  =  " << (Pos3 != Pos1) << endl;
 
-    Vec3 = {1,1,1};
-    cout << "Vec3  <  Vec1  =  " << (Vec3 < Vec1) << endl;
+	Pos3 = {1,1,1};
+	cout << "Pos3  <  Pos1  =  " << (Pos3 < Pos1) << endl;
 
-    cout << "Vec1[n]  =" << "  Vec1[0]=" << Vec1[0] << "  Vec1[1]=" << Vec1[1] << "  Vec1[2]=" << Vec1[2] << endl;
+	cout << "Pos1[n]  =" << "  Pos1[0]=" << Pos1[0] << "  Pos1[1]=" << Pos1[1] << "  Pos1[2]=" << Pos1[2] << endl;
+
+
+
+	cout << "\n\n\n";
+
+	MathLibrary::Vector4 Rot1{1,2,3,4};
+	MathLibrary::Vector4 Rot2{5,6,7,8};
+	MathLibrary::Vector4 Rot3{};
+
+	Rot3 = Rot1 + Rot2;
+	cout << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "," << "  +  " << Rot2.x << "," << Rot2.y << "," << Rot2.z << "," << Rot2.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = Rot1 - Rot2;
+	cout << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "," << "  -  " << Rot2.x << "," << Rot2.y << "," << Rot2.z << "," << Rot2.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = Rot1 * Rot2;
+	cout << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "," << "  *  " << Rot2.x << "," << Rot2.y << "," << Rot2.z << "," << Rot2.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = Rot1 * 5;
+	cout << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "," << "  *  " << "5f" << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = Rot1 / 5;
+	cout << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "," << "  /  " << "5f" << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+
+	cout << "Rot3 from here on will always be set to 1,1,1,1 before operations." << endl;
+
+	Rot3 = {1,1,1,1};
+	Rot3 += Rot1;
+	cout << " +=  " << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	Rot3 -= Rot1;
+	cout << " -=  " << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	Rot3 *= Rot1;
+	cout << " *=  " << Rot1.x << "," << Rot1.y << "," << Rot1.z << "," << Rot1.w << "  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	Rot3 *= 5;
+	cout << " *=  5  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	Rot3 /= 5;
+	cout << " /=  5  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	-Rot3;
+	cout << " -Rot  =  " << Rot3.x << "," << Rot3.y << "," << Rot3.z << "," << Rot3.w << endl;
+
+	Rot3 = {1,1,1,1};
+	cout << "Rot3  ==  Rot1  =  " << (Rot3 == Rot1) << endl;
+
+	Rot3 = {1,1,1,1};
+	cout << "Rot3  !=  Rot1  =  " << (Rot3 != Rot1) << endl;
+
+	Rot3 = {1,1,1,1};
+	cout << "Rot3  <  Rot1  =  " << (Rot3 < Rot1) << endl;
+
+	cout << "Rot1[n]  =" << "  Rot1[0]=" << Rot1[0] << "  Rot1[1]=" << Rot1[1] << "  Rot1[2]=" << Rot1[2] << "  Rot1[3]=" << Rot1[3] << endl;
+
 
 }
 
